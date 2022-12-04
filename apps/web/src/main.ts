1b12fbd03e5cf6e72f1368223ba90ffa1a405751
Hai Nguyen <dinhhai281@gmail.com>
2022-12-04 20:05:09 +0700

    refactor(web): chane back to use webpack
    - tailwindcss not work with es-build

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,6 +1,10 @@
+import { enableProdMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 
 (async () => {
   const config = await fetch('assets/config.json').then(r => r.json());
+  if (process.env['NODE_ENV'] === 'production') {
+    enableProdMode();
+  }
   void AppComponent.bootstrap(config);
 })();
