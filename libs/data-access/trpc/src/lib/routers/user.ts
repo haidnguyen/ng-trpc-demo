f2fb21a43e0d9e5bab14b281315ba474deba8643
Hai Nguyen <dinhhai281@gmail.com>
2022-12-04 19:38:16 +0700

    feat: integrate prisma

--- libs/data-access/trpc/src/lib/routers/user.ts
+++ libs/data-access/trpc/src/lib/routers/user.ts
@@ -1,17 +1,17 @@
 import { z } from 'zod';
 import { procedure, router } from '../trpc';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const userRouter = router({
-  getUser: procedure.input(z.string()).query(req => {
-    return {
-      id: req.input,
-      name: 'Hai',
-    };
-  }),
-  getInfo: procedure.input(z.string()).query(req => {
-    return {
-      name: req.input,
-      test: 'aaa',
-    };
+  userCreate: procedure.input(z.object({ name: z.string().optional(), email: z.string() })).mutation(async req => {
+    const user = await prisma.user.create({
+      data: {
+        email: req.input.email,
+        name: req.input.name,
+      },
+    });
+    return user;
   }),
 });
--- libs/web/feature-home/src/lib/web-feature-home/web-feature-home.component.ts
+++ libs/web/feature-home/src/lib/web-feature-home/web-feature-home.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { fromProcedure, injectClient } from '@conduit/web/trpc';
 
 @Component({
   selector: 'conduit-web-feature-home',
@@ -9,4 +10,15 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./web-feature-home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WebFeatureHomeComponent {}
+export class WebFeatureHomeComponent implements OnInit {
+  private readonly client = injectClient();
+
+  ngOnInit() {
+    fromProcedure(this.client.user.userCreate.mutate)({
+      name: 'Hai',
+      email: 'dinhhai281@gmail.com',
+    }).subscribe(response => {
+      console.log(response);
+    });
+  }
+}
